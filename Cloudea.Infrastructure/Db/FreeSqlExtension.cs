513925ae463e47d86ec152d2a0608690fd9686ca
Cloudea <2025140062@qq.com>
2023-08-11 16:01:29 +0800

    T

---
+++ Cloudea.Infrastructure/Db/FreeSqlExtension.cs
@@ -0,0 +1,180 @@
+﻿using Cloudea.Infrastructure.Models;
+using FreeSql.Internal.Model;
+using FreeSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloudea
+{
+    public static class FreeSqlExtendsion
+    {
+        /// <summary>
+        /// 同步表结构
+        /// </summary>
+        /// <typeparam name="TOption"></typeparam>
+        /// <param name="fsql"></param>
+        /// <param name="option"></param>
+        public static void SyncStructure(this IFreeSql fsql, IEnumerable<Type> types)
+        {
+            fsql.CodeFirst.SyncStructure(types.ToArray());
+        }
+
+
+        /// <summary>
+        /// 通过sql查找条数( "select count(*) from xxx" )
+        /// </summary>
+        /// <param name="that"></param>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static int FindCountBySql(this IFreeSql that, string sql, object param = null)
+        {
+            var table = that.Ado.ExecuteDataTable(sql, param);
+            object rawCount = table.Rows[0][0];
+            if (int.TryParse(rawCount.ToString(), out int count))
+            {
+                return count;
+            }
+            throw new Exception("parse count error!");
+        }
+        /// <summary>
+        ///  通过sql查找条数( "select count(*) from xxx" )
+        /// </summary>
+        /// <param name="that"></param>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static async Task<int> FindCountBySqlAsync(this IFreeSql that, string sql, object param = null)
+        {
+            var table = await that.Ado.ExecuteDataTableAsync(sql, param);
+            object rawCount = table.Rows[0][0];
+            if (int.TryParse(rawCount.ToString(), out int count))
+            {
+                return count;
+            }
+            throw new Exception("parse count error!");
+        }
+
+        /// <summary>
+        /// 获得分页数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="select"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static async Task<PageResponse<T>> ToPageListAsync<T>(this ISelect<T> select, PageRequest request)
+        {
+            return await select.ToPageList(request.Page, request.Limit);
+        }
+
+        /// <summary>
+        /// 获得分页数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="select"></param>
+        /// <param name="page"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static async Task<PageResponse<T>> ToPageList<T>(this ISelect<T> select, int page, int pageSize)
+        {
+            if (page <= 0)
+            {
+                var total = await select.CountAsync();
+                var data = await select.ToListAsync();
+
+                return new PageResponse<T>()
+                {
+                    Rows = data,
+                    Total = total
+                };
+            }
+            else
+            {
+                dynamic s = select;
+                if (s._orderby == null)
+                {
+                    var tables = s._tables as List<SelectTableInfo>;
+                    var pktb = tables.Where(a => a.Table.Primarys.Any()).FirstOrDefault();
+                    if (pktb != null)
+                    {
+                        select = select.OrderByPropertyName(pktb?.Table.Primarys.First().Attribute.Name);
+                    }
+                    else
+                    {
+                        select = select.OrderByPropertyName(tables.First().Table.Columns.First().Value.Attribute.Name);
+                    }
+                }
+                var list = await select.Count(out long total).Page(page, pageSize).ToListAsync();
+                return new PageResponse<T>()
+                {
+                    Total = total,
+                    Rows = list
+                };
+            }
+        }
+
+        /// <summary>
+        /// 通过Id查找
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="select"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static async Task<T> FindByIdAsync<T>(this ISelect<T> select, long id) where T : EntityBase
+        {
+            return await select.Where(t => t.Id == id).FirstAsync();
+        }
+
+        /// <summary>
+        /// 通过sql查询列表
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fsql"></param>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static List<T> FindListBySql<T>(this IFreeSql fsql, string sql, object param = null) where T : class
+        {
+            return fsql.Ado.Query<T>(sql, param);
+        }
+
+        /// <summary>
+        /// 通过sql查询列表 异步
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fsql"></param>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static async Task<List<T>> FindListBySqlAsync<T>(this IFreeSql fsql, string sql, object param = null) where T : class
+        {
+            return await fsql.Ado.QueryAsync<T>(sql, param);
+        }
+
+        /// <summary>
+        /// 通过sql查询单个实体 异步
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fsql"></param>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static async Task<T> FindEntityBySqlAsync<T>(this IFreeSql fsql, string sql, object param = null) where T : class
+        {
+            return await fsql.Ado.QuerySingleAsync<T>(sql, param);
+        }
+
+        /// <summary>
+        /// 通过sql查询单个实体
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fsql"></param>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public static T FindEntityBySql<T>(this IFreeSql fsql, string sql, object param = null) where T : class
+        {
+            return fsql.Ado.QuerySingle<T>(sql, param);
+        }
+    }
--- Cloudea.Web/Controllers/MyController.cs
+++ Cloudea.Web/Controllers/MyController.cs
@@ -20,12 +20,6 @@ namespace Cloudea.Web.Controllers
             this.service = service;
         }
 
-        /*[HttpGet]
-        public async Task<int> Reee()
-        {
-            return service.Send();
-        }*/
-
         [HttpGet("{a}/{b}")]
         public ActionResult<int> Add(int a, int b)
         {
@@ -34,67 +28,11 @@ namespace Cloudea.Web.Controllers
         }
 
         [HttpGet]
-        public async Task<List<MyJst>> MyJsts()
+        public async Task<string> HowAreYou()
         {
-            var list = new List<MyJst>();
-            using(HttpClient client = new HttpClient())
-            {
-                var response = await client.GetAsync("http://localhost:53812/api/TdRequest/GetListABC");
-                list = await response.Content.ReadFromJsonAsync<List<MyJst>>();
-            }
-            /* var list = new PageResponse<MyJst>();
-             using(HttpClient client = new HttpClient())
-             {
-                 try
-                 {
-                     var response = await client.GetAsync("http://localhost:53812/api/TdRequest/GetListABC");
-                     string content = await response.Content.ReadAsStringAsync();
-                     if (response.StatusCode == HttpStatusCode.OK)
-                     {
-                         list = JsonConvert.DeserializeObject<PageResponse<MyJst>>(content);
-                     }
-                     else
-                     {
-                         return list;
-                     }
-                 }
-                 catch(Exception ex)
-                 {
-                     return list;
-                 }
-             }*/
-            return list;
+            
+            return "你好";
         }
     }
 
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-    public class MyJst
-    {
-        //业务编号
-        public string BS_CODE { get; set; }
-        //主提单号
-        public string CGO_MBL_NO { get; set; }
-        //签单类型
-        public string CGO_MBL_TYPE { get; set; } = "1";
-        //签回人员
-        public string QH_EXECUTEER { get; set; }
-        //签回日期
-        public DateTime? QH_CRT_DATE { get; set; }
-        //实际签回人员
-        public string QH_REAL_EXER { get; set; }
-        //扫描人员
-        public string SM_EXECUTEER { get; set; }
-        //扫描日期
-        public DateTime? SM_CRT_DATE { get; set; }
-        //实际扫描人员
-        public string SM_REAL_EXER { get; set; }
-        //接收状态
-        public string RECEIVE_STATUS { get; set; } = "0";
-        //接收人员
-        public string RECEIVER { get; set; } = "CCZ蔡蔡子";
-    }
 }
