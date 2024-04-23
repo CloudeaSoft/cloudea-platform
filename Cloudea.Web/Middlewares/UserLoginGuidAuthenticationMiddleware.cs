87c06ea95ba3cf6b3d78d757c8690a25ce9d4876
Cloudea <2025140062@qq.com>
2024-04-23 22:54:34 +0800

    2024-04-23: Resolve NullReference Error In Unique Login Middleware.

--- Cloudea.Web/Middlewares/UserLoginGuidAuthenticationMiddleware.cs
+++ Cloudea.Web/Middlewares/UserLoginGuidAuthenticationMiddleware.cs
@@ -24,34 +24,36 @@ namespace Cloudea.Web.Middlewares
             if (request.Headers.TryGetValue("Authorization", out _))
             {
                 Claim? userIdClaim = context.User.Claims.FirstOrDefault(t => t.Type == JwtClaims.USER_ID);
-                string userId = userIdClaim!.Value;
-
-                var userGuidClaim = context.User.Claims.FirstOrDefault(t => t.Type == JwtClaims.USER_LOGIN_GUID);
-                string guid = userGuidClaim!.Value;
-
-                if (!string.IsNullOrWhiteSpace(userId) && !string.IsNullOrWhiteSpace(guid))
+                if (userIdClaim is not null)
                 {
-                    // 两个不为空的时候判断只能有同一个人同时登录
-                    var savedGuid = authUserService.GetUserLoginGuid(userId);
+                    string userId = userIdClaim!.Value;
 
-                    if (string.IsNullOrWhiteSpace(savedGuid))
+                    var userGuidClaim = context.User.Claims.FirstOrDefault(t => t.Type == JwtClaims.USER_LOGIN_GUID);
+                    string guid = userGuidClaim!.Value;
+
+                    if (!string.IsNullOrWhiteSpace(userId) && !string.IsNullOrWhiteSpace(guid))
                     {
-                        // 如果为空 设置当前的guid保存
-                        authUserService.SetUserLoginGuid(userId, guid);
-                    }
-                    else
-                    {
-                        // 不为空 如果不相等说明不是当前登录的用户 返回409 Conflict
-                        if (savedGuid != guid)
+                        // 两个不为空的时候判断只能有同一个人同时登录
+                        var savedGuid = authUserService.GetUserLoginGuid(userId);
+
+                        if (string.IsNullOrWhiteSpace(savedGuid))
                         {
-                            context.Response.Headers.Append("CLOUDEA-USER-LOGOUT", "ONE-USER-LIMIT");
-                            context.Response.StatusCode = StatusCodes.Status409Conflict;
-                            return;
+                            // 如果为空 设置当前的guid保存
+                            authUserService.SetUserLoginGuid(userId, guid);
+                        }
+                        else
+                        {
+                            // 不为空 如果不相等说明不是当前登录的用户 返回409 Conflict
+                            if (savedGuid != guid)
+                            {
+                                context.Response.Headers.Append("CLOUDEA-USER-LOGOUT", "ONE-USER-LIMIT");
+                                context.Response.StatusCode = StatusCodes.Status409Conflict;
+                                return;
+                            }
                         }
                     }
                 }
             }
-
             await _next(context);
         }
     }
